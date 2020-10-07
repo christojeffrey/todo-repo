67749c2c3e9c003df7df84d3583665902b50b36f
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2020-10-07 11:40:40 +0700

    nambah i image, buat button2 html sama function add

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log('weweweewe');
+document.getElementById("add-button").addEventListener("click",add_button_clicked);
+function add_button_clicked(){
+    console.log("add button clicked");
+    var todo_text = document.getElementById("text-area").value;
+    console.log(todo_text);
+    document.getElementById("text-area").value = "";
+
+    //creating list
+    var list = document.createElement("li");
+    list.classList.add("list");
+    var node_text = document.createTextNode(todo_text);
+    list.appendChild(node_text);
+
+    //creating trash-button,appending it to list
+    var trash_button = document.createElement("button");
+    trash_button.classList.add("trash-button");
+    node_text = document.createTextNode("trash");
+    trash_button.appendChild(node_text);
+    list.appendChild(trash_button);
+
+    //creating complete-button, appending it to list
+    var complete_button = document.createElement("button");
+    complete_button.classList.add("complete-button");
+    node_text = document.createTextNode("complete");
+    complete_button.appendChild(node_text);
+    list.appendChild(complete_button);
+
+    //appending list to todo-list
+    var todo_list = document.getElementById("todo-list");
+    todo_list.appendChild(list);
+
+
+ 
+    }
+document.getElementsByClassName("trash-button").addEventListener("click", trashed);
+function trashed(){
+    console.log("trashed!");
+}
